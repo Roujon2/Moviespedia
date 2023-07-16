7d9effacee4392cf358ed071ce8c41d93a772fd3
Roujon2 <jonroulet@gmail.com>
2023-07-16 16:37:20 -0300

    API Fetching testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+var test_key = process.env.REACT_APP_TEST_API_KEY;
+
 
 function App() {
+
+  const [data, setData] = useState(null);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  useEffect(() => {
+    // Api from https://www.geeksforgeeks.org/how-to-fetch-data-from-an-api-in-reactjs/
+    fetch(test_key)
+      .then(response => response.json())
+      .then(json => {
+        setData(json);
+        setDataLoaded(true);})
+      .catch(error => console.error(error));
+  }, []);
+
+  if (!dataLoaded) {
+    return(
+      <div>
+        <h1>Loading... {test_key}</h1>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetching Data from an API</h1>
+      {
+        data.map((item) => (
+          <ol key={item.id}>
+            User_Name: {item.username},
+            Full_Name: { item.name },
+            User_Email: { item.email }
+          </ol>
+        ))
+      }
     </div>
   );
 }
