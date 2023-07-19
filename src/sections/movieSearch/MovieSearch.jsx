6d1340a3a4e0dfe770579ce39153851ab864e5b3
Roujon2 <jonroulet@gmail.com>
2023-07-19 17:43:19 -0300

    Change search by movie ID to movie title - added loading

--- src/sections/movieSearch/MovieSearch.jsx
+++ src/sections/movieSearch/MovieSearch.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './movieSearch.css'
 import { useState, useEffect } from 'react'
 import { axiosRequest } from '../../api.js';
+import loading from '../../assets/loading_spinner.gif';
+
+import nebula from '../../assets/Helix Nebula.jpg';
 
 
 var api_key = process.env.REACT_APP_TMDB_API_KEY;
@@ -9,30 +12,34 @@ var api_key = process.env.REACT_APP_TMDB_API_KEY;
 
 const MovieSearch = () => {
     // Tracking the input of the user
-    const [movieId, setMovieId] = useState('');
-    const [posterPath, setPosterPath] = useState("https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg");
+    const [movieQuery, setMovieQuery] = useState('');
+    const [posterPath, setPosterPath] = useState(nebula);
 
 
     const [data, setData] = useState(null);
 
     const handleInputChange = (event) => {
-        setMovieId(event.target.value.toString());
+        setMovieQuery(event.target.value.toString());
     }
 
     // When the submit button is clicked
     const getData = async (event) => {
+        // Set the loading screen
+        setPosterPath(loading);
+
         // Prevents the page from reloading when the form is submitted
         event.preventDefault();
 
         // Reset the text input field
-        setMovieId('');
+        setMovieQuery('');
 
-        axiosRequest.get(`/movie/${movieId}?api_key=${api_key}`)
+        axiosRequest.get(`/search/movie?query=${movieQuery}&include_adult=false&language=en-US&api_key=${api_key}`)
         .then((response) => {
             setData(response.data);
+            console.log(response);
 
             // Check if the poster_path is available in the response
-            const posterPath = response.data.poster_path || '';
+            const posterPath = response.data.results[0].poster_path || '';
             setPosterPath(posterPath ? "https://image.tmdb.org/t/p/w500" + posterPath : '');
         })
         .catch((err) => {
@@ -45,10 +52,10 @@ const MovieSearch = () => {
         <section>
             <div className="container movie_search_container">
                 <h1>Movie Search</h1>
-                <h3>Search movie with ID</h3>
+                <h3>Search movie Title</h3>
                 <div className='fields_container'>
                     <form action="" className='fields' onSubmit={getData}>
-                        <input type="number" onChange={handleInputChange} name="movie_id" placeholder="Enter movie ID" value={movieId} required/>
+                        <input type="text" onChange={handleInputChange} name="movie_title" placeholder="Enter movie title" value={movieQuery} required/>
                         <button type='submit' className='btn btn-primary'>Search</button>
                     </form>
 
