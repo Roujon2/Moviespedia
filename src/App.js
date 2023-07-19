d755c9a94aeabffa7eae33d5205bbf0c387bf20c
Roujon2 <jonroulet@gmail.com>
2023-07-19 11:00:30 -0300

    Css changes and removed unused code

--- src/App.js
+++ src/App.js
@@ -10,48 +10,9 @@ function App(){
   return(
     <div>
       <MovieSearch/>
-      <Fetch/>
     </div>
   )
 }
 
-function Fetch() {
-
-  const [data, setData] = useState(null);
-  const [dataLoaded, setDataLoaded] = useState(false);
-
-  useEffect(() => {
-    // Api from https://www.geeksforgeeks.org/how-to-fetch-data-from-an-api-in-reactjs/
-    fetch(test_key)
-      .then(response => response.json())
-      .then(json => {
-        setData(json);
-        setDataLoaded(true);})
-      .catch(error => console.error(error));
-  }, []);
-
-  if (!dataLoaded) {
-    return(
-      <div>
-        <h1>Loading... {test_key}</h1>
-      </div>
-    )
-  }
-
-  return (
-    <div className="App">
-      <h1>Fetching Data from an API</h1>
-      {
-        data.map((item) => (
-          <ol key={item.id}>
-            User_Name: {item.username},
-            Full_Name: { item.name },
-            User_Email: { item.email }
-          </ol>
-        ))
-      }
-    </div>
-  );
-}
 
 export default App;
